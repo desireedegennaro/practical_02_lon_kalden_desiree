55ec6ab3c71e8712642f7b1bfc043d23366cf913
kaldenh <kaldenharp@gmail.com>
2025-03-23 17:12:24 -0400

    working chroma john

--- chroma.py
+++ chroma.py
@@ -1,22 +1,53 @@
+# from pydantic_settings import BaseSettings
 import chromadb
-from pydantic_settings import BaseSettings
+import ollama
 
 # Initialize the Chroma client
 # persist_directory = "C:\Users\kalde\Downloads\Northeastern\Spring 2025\DS 4300"
+
+# Initialize the Chroma client with the persist directory
 client = chromadb.Client()
+
 
 # Create a collection (if it doesn't exist)
 collection_name = "4300-chroma"
 collection = client.create_collection(collection_name)
 
 
-documents = ["This is document 1", "This is document 2", "This is document 3"]
-ids = ["id1", "id2", "id3"]
+documents = ["This is document 1", "This is document 2", "This is document 3", 'This is a query document about florida']
+ids = ["id1", "id2", "id3", 'id4']
 # metadatas = [{"source": "source1"}, {"source": "source2"}, {"source": "source3"}] # Optional metadata
 collection.add(
         documents=documents,
         ids=ids,
         # metadatas=metadatas # Optional
     )
+query = input('what is your query?')
+query_res = collection.query(
+    query_texts=[query], # Chroma will embed this for you
+    n_results=3 # how many results to return
+)
 
-print(collection)
+
+response = ollama.chat(
+    model='mistral',
+    messages=[
+        {
+            'role':'system',
+            'content':query_res['documents'][0][0]
+        }, 
+        {
+            'role':'system',
+            'content':query_res['documents'][0][1]
+        }, 
+        {
+            'role':'system',
+            'content':query_res['documents'][0][2]
+        },
+        {
+            'role':'user',
+            'content':query
+        }
+    ]
+)
+print(response['message'])
--- sample_code.py
+++ sample_code.py
@@ -5,8 +5,11 @@ import redis
 import numpy as np
 from redis.commands.search.query import Query
 
+
+ollama.pull("nomic-embed-text")
+
 # Initialize Redis connection
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
+redis_client = redis.Redis(host="localhost", port=6379, db=0)#, password='mypassword')
 
 VECTOR_DIM = 768
 INDEX_NAME = "embedding_index"
