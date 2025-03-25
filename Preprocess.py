a4444aa9062db1a997a954acb68eecfb97f00998
desireedegennaro <degennaro.d@northeastern.edu>
2025-03-25 18:49:57 -0400

    chunking

--- Preprocess.py
+++ Preprocess.py
@@ -4,8 +4,9 @@ import os
 
 
 
-def read_data(chunk_size=200, overlap=50):
+def read_data(chunk_sizes=[200, 500, 1000], overlaps=[0, 50, 100]):
     filepath = "data\\"
+    word_docs = {}
 
     file_list = os.listdir(filepath)
     for file in file_list:
@@ -16,21 +17,23 @@ def read_data(chunk_size=200, overlap=50):
             for line in infile:
                 stripped = line.strip()
                 file_str += stripped
-        
+        chunking = {}
         ## chunking w overlap
-        word_docs = {}
-        prev_idx = 0
-        count = 1
-        while True:
-            if prev_idx+chunk_size > len(file_str): 
+        for chunk_size in chunk_sizes:
+            for overlap in overlaps:
+                chunks = []
+                prev_idx = 0
+                count = 1
+        while prev_idx < len(file_str):
+            if prev_idx+chunk_size >= len(file_str): 
                 chunk = file_str[prev_idx:]
             else:  
                 chunk = file_str[prev_idx:prev_idx+chunk_size]
-            
+            chunks.append(chunk)
             prev_idx = (prev_idx+chunk_size)-overlap
-            word_docs[file+f'chunk{count}'] = chunk
+        chunks = chunking[f"chunk{chunk_size}_overlap{overlap}"] 
+    word_docs[file] = chunking
 
     return word_docs
-
 
 read_data()
