3eec1f9f4db51e353c111b358b86891290f8d5da
kaldenh <kaldenharp@gmail.com>
2025-04-01 12:51:13 -0400

    new experiment!!!! yasss

---
+++ new_experiment.py
@@ -0,0 +1,84 @@
+import time
+import csv
+import ollama
+import chromadb
+import redis
+from qdrant_client import QdrantClient
+from chroma import store_embeddings as chroma_chat
+from redis_driver import store_embedding as redis_chat
+from qdrant import store_embeddings as qdrant_chat
+from sentence_transformers import SentenceTransformer
+from Preprocess import read_data 
+import pandas as pd
+
+
+# embedding models to be tested
+EMBEDDING_MODELS = [
+    "sentence-transformers/all-MiniLM-L6-v2",
+    "sentence-transformers/all-mpnet-base-v2",
+    "hkunlp/instructor-xl"
+]
+
+# llms to be tested
+LLM_MODELS = ["llama2", "mistral"]
+
+# define chunk sizes and overlaps
+CHUNK_SIZES = [200, 500, 1000]
+OVERLAPS = [0, 50, 100]
+
+QUERY_TEXTS = ["What is redis?", "What is an AVL tree?", "How do document databases like MongoDB differ from relational databases?", "What are tradeoffs between B+ Trees and LSM?"]
+
+# write CSV headers
+EXPORT_COLS = [
+        "Chunk Size", "Overlap", "Embedding Model", "Query", "Vector DB",
+        "Query Time (s)", "Memory Used", "LLM Model", "LLM Response"
+    ]
+
+
+# query, model, word_docs, embed_model
+def run_experiment(db_name):
+    # loop through chunk sizes and overlaps
+    export_df = pd.DataFrame(columns=EXPORT_COLS)
+
+    if db_name == 'chroma':
+        func = chroma_chat
+        export_name = 'results_chroma.csv'
+    elif db_name == 'qdrant':
+        func = qdrant_chat
+        export_name = 'results_qdrant.csv'
+    elif db_name == 'redis':
+        func = redis_chat
+        export_name = 'results_redis.csv'
+
+
+    for chunk_size in CHUNK_SIZES:
+        for overlap in OVERLAPS:
+            print(f"\nProcessing chunk size {chunk_size}, overlap {overlap}")
+
+            #preprocess
+            word_docs = read_data(chunk_sizes=[chunk_size], overlaps=[overlap])
+
+            # going through each model, embedding model, and query for chunk size and overlap
+            for model in LLM_MODELS:
+                for embedding_model in EMBEDDING_MODELS:
+                    for query in QUERY_TEXTS:
+                        query_result, run_time, memory = func(query, model, word_docs, embedding_model)
+                        print('query:', query)
+                        print('embedding_model', embedding_model)
+                        print('model', model)
+                        concat_df = pd.DataFrame(columns = EXPORT_COLS, data=[chunk_size, overlap, embedding_model,
+                                                                               query, db_name, run_time, memory, model, query_result])
+                        export_df = pd.concat([export_df, concat_df])
+        
+
+    export_df.to_csv(export_name)
+
+
+
+def main():
+    run_experiment('chroma')
+
+
+if __name__ == '__ main __':
+    main()
+    
